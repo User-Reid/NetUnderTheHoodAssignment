ac2cf4785b4271b2597a19305fbc7e44ee33cc04
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-09 20:51:06 -0500

    first commit

---
+++ NewSolution/FastTableDataBuilder.cs
@@ -0,0 +1,15 @@
+﻿using CsvDataAccess.CsvReading;
+using CsvDataAccess.Interface;
+using CsvDataAccess.OldSolution;
+
+namespace CsvDataAccess.NewSolution;
+
+public class FastTableDataBuilder : ITableDataBuilder
+{
+   public ITableData Build(CsvData csvData)
+    {
+        
+
+        return new TableData(csvData.Columns, resultRows);
+    }
+}
